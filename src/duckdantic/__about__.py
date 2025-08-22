5c2d7f57ec336e566fdc3ef840b852a8601228b5
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-08-22 17:26:57 -0400

    ci: add documentation build test workflow and update mkdocs config

--- src/duckdantic/__about__.py
+++ src/duckdantic/__about__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.6'
-__version_tuple__ = version_tuple = (1, 0, 6)
+__version__ = version = '1.0.7'
+__version_tuple__ = version_tuple = (1, 0, 7)
 
 __commit_id__ = commit_id = None
