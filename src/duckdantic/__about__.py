68eed961e48f49f80d060747c581657dbeb60c81
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-08-22 15:54:55 -0400

    feat: add CONTRIBUTING guide and examples documentation for Duckdantic, enhancing community engagement and usage clarity

--- src/duckdantic/__about__.py
+++ src/duckdantic/__about__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.dev6+g4d42f1996.d20250822"
-__version_tuple__ = version_tuple = (0, 1, "dev6", "g4d42f1996.d20250822")
+__version__ = version = "0.1.dev13+g4e13b4efd.d20250822"
+__version_tuple__ = version_tuple = (0, 1, "dev13", "g4e13b4efd.d20250822")
 
 __commit_id__ = commit_id = None
